4628c92055fcec449ed85883a3a3b843a5124520
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2025-12-10 14:14:51 +0100

    add VS Code settings and extensions, PowerShell profile and Windows Terminal settings.

---
+++ PowerShell/profile.ps1
@@ -0,0 +1,8 @@
+Set-PSReadLineOption -PredictionViewStyle ListView -PredictionSource History -HistoryNoDuplicates -MaximumHistoryCount 10000
+
+oh-my-posh init pwsh | Invoke-Expression
+
+cls
+cls
+
+# winfetch
